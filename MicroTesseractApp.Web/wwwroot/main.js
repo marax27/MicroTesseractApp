96bb1a84a1b9bd69d129a53bdd29330449a2a99e
Kacper Tonia <kacpert314@gmail.com>
2022-10-16 13:27:13 +0200

    Tweak frontend

--- MicroTesseractApp.Web/wwwroot/main.js
+++ MicroTesseractApp.Web/wwwroot/main.js
@@ -1,13 +1,31 @@
+function createTextNode(tagName, innerText, classes = []) {
+  const element = document.createElement(tagName);
+  element.innerText = innerText;
+  classes.forEach((className) => element.classList.add(className));
+  return element;
+}
+
+function createParent(tagName, childNode) {
+  const element = document.createElement(tagName);
+  element.appendChild(childNode);
+  return element;
+}
+
 function updateOcrResults(ocrResult) {
   const outputElement = document.getElementById("ocrResults");
   const { filename, meanConfidence, text } = ocrResult;
 
-  outputElement.innerHTML = `
-    <h3>OCR Results</h3>
-    <p>Filename: ${filename}</p>
-    <p>Mean Confidence: ${meanConfidence}</p>
-    <p>Text: ${text}</p>
-  `;
+  const children = [
+    createParent("p", createTextNode("strong", filename)),
+    createParent(
+      "p",
+      createTextNode("strong", `Mean confidence: ${meanConfidence}`)
+    ),
+    createTextNode("div", text, ["text-results-block"]),
+  ];
+
+  outputElement.innerHTML = "";
+  children.forEach((child) => outputElement.appendChild(child));
 }
 
 function uploadFiles(formElement) {
